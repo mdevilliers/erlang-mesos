169f494ce171e4cb0a1c1b38e122e49b21c08964
Mark deVilliers <markdevilliers@gmail.com>
2016-02-04 08:08:38 +0000

    reorder test to debug travis test issue

--- test/mesos_scheduler_integration_tests.erl
+++ test/mesos_scheduler_integration_tests.erl
@@ -32,6 +32,39 @@ inited_scheduler_can_not_be_reinited_test()->
 
     meck:unload(test_framework).
 
+crash_in_scheduler_closes_nif_connections_gracefully_test()->
+    
+    % naughty framework
+    meck:new(test_framework_two, [non_strict]), 
+
+    FrameworkInfo = #'FrameworkInfo'{user="", name="Erlang Test Framework 2"},
+    meck:expect(test_framework_two, init , fun(_) -> {FrameworkInfo, ?MASTER_LOCATION, []} end),
+    meck:expect(test_framework_two, registered , fun(_FrameworkID, _MasterInfo,_State) -> 
+        io:format(user, "registered called",[]),
+        meck:exception(error, boom) end),
+    meck:expect(test_framework_two, resourceOffers , fun(_Offer, State) -> {ok,State} end),
+
+    {ok,_} = scheduler:start(test_framework_two, ?MASTER_LOCATION),
+ 
+    io:format(user, "waiting for crash~n", []),
+    timer:sleep(500), % wait for crash
+    io:format(user, "starting up again~n", []),
+    meck:unload(test_framework_two),
+
+    % nice framework
+    meck:new(test_framework_two, [non_strict]), 
+    meck:expect(test_framework_two, init , fun(_) -> { FrameworkInfo, ?MASTER_LOCATION, []} end),
+    meck:expect(test_framework_two, registered , fun(_FrameworkID, _MasterInfo, State) -> {ok,State} end),
+    meck:expect(test_framework_two, resourceOffers , fun(_Offer, State) -> {ok,State} end),
+
+    {ok,_} = scheduler:start(test_framework_two, ?MASTER_LOCATION),
+
+    {ok, driver_stopped} = scheduler:stop(0),
+    ok = scheduler:destroy(),
+    
+    meck:unload(test_framework_two).
+
+
 unknown_message_to_scheduler_will_not_crash_scheduler_test() ->
 
     meck:new(test_framework, [non_strict]), 
@@ -47,36 +80,6 @@ unknown_message_to_scheduler_will_not_crash_scheduler_test() ->
     CheekyPid = whereis(scheduler),
     CheekyPid ! {booya},
     CheekyPid = whereis(scheduler),
-
-    {ok, driver_stopped} = scheduler:stop(0),
-    ok = scheduler:destroy(),
-    
-    meck:unload(test_framework).
-
-crash_in_scheduler_closes_nif_connections_gracefully_test()->
-    
-    % naughty framework
-    meck:new(test_framework, [non_strict]), 
-
-    FrameworkInfo = #'FrameworkInfo'{user="", name="Erlang Test Framework"},
-    meck:expect(test_framework, init , fun(_) -> { FrameworkInfo, ?MASTER_LOCATION, []} end),
-    meck:expect(test_framework, registered , fun(_FrameworkID, _MasterInfo,_State) -> meck:exception(error, boom) end),
-    meck:expect(test_framework, resourceOffers , fun(_Offer, State) -> {ok,State} end),
-
-    {ok,_} = scheduler:start(test_framework, ?MASTER_LOCATION),
- 
-    io:format(user, "waiting for crash~n", []),
-    timer:sleep(4500), % wait for crash
-    io:format(user, "starting up again~n", []),
-    meck:unload(test_framework),
-
-    % nice framework
-    meck:new(test_framework, [non_strict]), 
-    meck:expect(test_framework, init , fun(_) -> { FrameworkInfo, ?MASTER_LOCATION, []} end),
-    meck:expect(test_framework, registered , fun(_FrameworkID, _MasterInfo, State) -> {ok,State} end),
-    meck:expect(test_framework, resourceOffers , fun(_Offer, State) -> {ok,State} end),
-
-    {ok,_} = scheduler:start(test_framework, ?MASTER_LOCATION),
 
     {ok, driver_stopped} = scheduler:stop(0),
     ok = scheduler:destroy(),
